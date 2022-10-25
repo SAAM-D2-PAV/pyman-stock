d5fcd5318f64789150ec5825d82addd5c89c7313
Mathieu Le Ray <leraymathieu@gmail.com>
2022-10-25 17:11:34 +0200

    Codage du composant Task.js

--- src/components/Task.js
+++ src/components/Task.js
@@ -8,28 +8,65 @@ import Navigation from '../components/Navigation';
   
 const Task = () => {
     //Variables et Fonctions du composant
-        
-    //Variable tasksData (tableau vide) -> stockage des taches récupérées par axios
-    const [taskData,setTaskData] = useState([]);
+
     // We can use the `useParams` hook here to access
     // the dynamic pieces of the URL.
     const {id} = useParams();
+
+    //Variable taskData (tableau vide) -> stockage de la tache récupérée par axios
+    const [taskData,setTaskData] = useState([]);
+    //Variable projectData (tableau vide) -> stockage du projet lié à la tâche par axios
+   
+   
     //Requète vers API
-    const getTask = () => {
+    const getData = () => {
         axios
         //On récupère la tache
         .get(`http://127.0.0.1:8000/api/tasks/${id}`)
         //Puis on les charge dans tasksData via setTasksData
-        .then((res)=>setTaskData(res.data['hydra:member']));
+        .then(
+
+            (res)=>setTaskData(res.data)
+      
+
+        )
     }
-    // Le useEffect se joue lorsque le composant est monté au chargement de la page
-    // Ici on lance la fonction getTask
-    useEffect(() => getTask(), [])
+
+   useEffect( () => getData(), [] ) 
+   
 
     return (
-        <div>
+        <div className='task'>
             <Navigation/> 
-            <h5>Tache n°{id}</h5>
+            <div className="container text-center">
+                <h3>Tâche {taskData.id}</h3>
+                <h4 className='red_flag'> {taskData.name} </h4>
+                <h6 className="card-subtitle mb-2 imperial_primer">projet :  </h6>
+                <div className="row">
+                    <div className="col-6">
+                        <div className="card">
+                           
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                            <ul className="list-group list-group-flush">
+                                <li className="list-group-item">An item</li>
+                                <li className="list-group-item">A second item</li>
+                                <li className="list-group-item">A third item</li>
+                            </ul>
+                            <div className="card-body">
+                                <a href="#" className="card-link">Card link</a>
+                                <a href="#" className="card-link">Another link</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-6">
+                        <div className="picture col-3"></div>
+                    </div>
+                </div>
+            </div>
+            
         </div>
     );
 };
--- src/components/ThumbnailTask.js
+++ src/components/ThumbnailTask.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const ThumbnailTask = ({task}) => {
     //Variables et Fonctions du composant
     //Variable tasksData (tableau vide) -> stockage des taches récupérées par axios
-    const [taskData,setTaskData] = useState([]);
+    //const [taskData,setTaskData] = useState([]);
     //Formatage de la date
     const dateFormater = (date) => {
         let newDate = new Date(date).toLocaleDateString("fr-FR", {
@@ -23,21 +23,21 @@ const ThumbnailTask = ({task}) => {
         })
         return newHour;
     }
-    //Fonction onclick affichage du tâche
-    const activateTask = (id) => {
-        axios
-        //On récupère la tache
-        .get("http://127.0.0.1:8000/api/tasks/",{id})
-        //Puis on les charge dans tasksData via setTasksData
-        .then((res)=>setTaskData(res.data['hydra:member']));
+    // //Fonction onclick affichage d'une tâche
+    // const activateTask = (id) => {
+    //     axios
+    //     //On récupère la tache
+    //     .get("http://127.0.0.1:8000/api/tasks/",{id})
+    //     //Puis on les charge dans tasksData via setTasksData
+    //     .then((res)=>setTaskData(res.data['hydra:member']));
         
-    }
+    // }
     return (
 
-        <div className='task col-md-6 col-xs-12'>
-        <div className="task-wrapper animate__animated animate__fadeIn p-3 border bg-light">
+        <div className='th-task col-md-6 col-xs-12'>
+        <div className="th-task-wrapper animate__animated animate__fadeIn p-3 border bg-light">
             <div className="">
-                <h5 className="task-title">{task.name}</h5>
+                <h5 className="th-task-title">{task.name}</h5>
                 <h6 className="card-subtitle mb-2 imperial_primer">Du {dateFormater(task.startDate)} à {hourFormater(task.startHour)} </h6>
                 <h6 className="card-subtitle mb-2 red_flag">Au {dateFormater(task.endDate)} à {hourFormater(task.endHour)}</h6>
                  <li key={task.id}> <Link to={`../tache/${task.id}`}> <i className="fa-solid fa-plus-minus"></i> {task.id} </Link> </li> 
